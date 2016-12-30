75bddf70896fe1cda16d585120ee065bec501413
Sam Gleske <sam.mxracer@gmail.com>
2016-12-30 01:28:32 -0800

    Add isUrlGood and logging mechanisms to functions

--- scripts/functions.groovy
+++ scripts/functions.groovy
@@ -1,6 +1,44 @@
 //functions used by sandscape.groovy and Sandscape plugins
 
+import java.text.DateFormat
+import java.util.Date
+import java.util.logging.Level
+import java.util.logging.Logger
+
+//list of available bindings
 getObjectValue = null
+isUrlGood = null
+sandscapeLogger = null
+sandscapePluginLogger = null
+
+/*
+   Define the bindings as functions.  The bindings had to be listed first in
+   order for recursion to work within closures.
+*/
+
+//sandscape logging mechanisms
+logger = Logger.getLogger('sandscape')
+sandscapeLevelLogger = { Level level, String message ->
+    String now = DateFormat.getDateTimeInstance().format(new Date())
+    logger.log(level, "${now} ${message}")
+}
+sandscapeLogger = { String message ->
+    sandscapeLevelLogger(Level.INFO, message)
+}
+sandscapeErrorLogger = { String message ->
+    sandscapeLevelLogger(Level.SEVERE, message)
+}
+sandscapePluginLogger = { Class clazz, String message ->
+    sandscapeLevelLogger(Level.INFO, "[${clazz.simpleName} sandscape plugin] ${message}")
+}
+sandscapePluginErrorLogger = { Class clazz, String message ->
+    sandscapeLevelLogger(Level.SEVERE, "[${clazz.simpleName} sandscape plugin] ${message}")
+}
+sandscapePluginLevelLogger = { Class clazz, Level level, String message ->
+    sandscapeLevelLogger(level, "[${clazz.simpleName} sandscape plugin] ${message}")
+}
+
+//a function for easily traversing objects
 getObjectValue = { Map object, String key, Object defaultValue ->
     if(key.indexOf('.') >= 0) {
         String key1 = key.split('\\.', 2)[0]
@@ -33,4 +71,27 @@ getObjectValue = { Map object, String key, Object defaultValue ->
 
     //nothing worked so just return default value
     return defaultValue
+}
+
+isUrlGood = { url ->
+    int code = -1
+    try {
+        code = new URL(url).openConnection().with {
+            requestMethod = 'HEAD'
+            //override HTTP headers Java sends for security reasons (discovered via tcpdump to http://example.com)
+            addRequestProperty "User-Agent", "Java"
+            addRequestProperty "Connection", "close"
+            addRequestProperty "Accept", "*/*"
+            //no network connection has been made up until this point
+            responseCode
+            //network connection is immediately closed
+        }
+    }
+    catch(MalformedURLException e) {
+        sandscapeErrorLogger(org.apache.commons.lang.exception.ExceptionUtils.getStackTrace(e))
+    }
+    catch(Exception e) {}
+    //2XX status is success - https://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.2
+    //return true if HTTP 2XX status
+    return ((int) code/100) == 2
 }
