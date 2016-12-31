14a5ba639ffa243d1627273294e51cc84444febc
Sam Gleske <sam.mxracer@gmail.com>
2016-12-30 22:58:31 -0800

    Update tests to use call

--- src/test/groovy/functionsTest.groovy
+++ src/test/groovy/functionsTest.groovy
@@ -17,18 +17,18 @@ class functionsTest extends GroovyTestCase {
     }
     @Test public void test_functions_getObjectValue() {
         Map example = [key1: [subkey1: 'string']]
-        assert 'string' == binding.getObjectValue.with { it(example, 'key1.subkey1', 'default') }
-        assert 'default' == binding.getObjectValue.with { it(example, 'key2.subkey1', 'default') }
-        assert 2 == binding.getObjectValue.with { it(example, 'key1.subkey1', 2) }
+        assert 'string' == binding.getObjectValue.call(example, 'key1.subkey1', 'default')
+        assert 'default' == binding.getObjectValue.call(example, 'key2.subkey1', 'default')
+        assert 2 == binding.getObjectValue.call(example, 'key1.subkey1', 2)
     }
     @Test public void test_functions_getObjectValue_false_bug() {
         Map example = [key1: [subkey1: 'false']]
-        assert false == binding.getObjectValue.with { it(example, 'key1.subkey1', false) }
-        assert false == binding.getObjectValue.with { it(example, 'key1.subkey1', true) }
+        assert false == binding.getObjectValue.call(example, 'key1.subkey1', false)
+        assert false == binding.getObjectValue.call(example, 'key1.subkey1', true)
     }
     @Test public void test_functions_getObjectValue_type_bug() {
         Map example = [key1: [subkey1: 'string'],key2: ["a", "b"]]
-        assert 'default' == binding.getObjectValue.with { it(example, 'key1', 'default') }
-        assert 'default' == binding.getObjectValue.with { it(example, 'key2', 'default') }
+        assert 'default' == binding.getObjectValue.call(example, 'key1', 'default')
+        assert 'default' == binding.getObjectValue.call(example, 'key2', 'default')
     }
 }
